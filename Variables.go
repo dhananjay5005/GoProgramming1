67cbe9c4e4f499ec181ae3f2e9f3e0774d238547
karn pawar <karnpawar2271@gmail.com>
2022-01-06 22:21:58 +0530

    Initial commit

---
+++ Variables.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//in golang we have two method to declare variables
+	var a int = 10
+	var name string = "Karn"
+	var boll bool = false
+	fmt.Println(a, name, boll)
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module GoProgramming
+
+go 1.17
